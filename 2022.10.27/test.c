00258014f5dbd8f38b2e9c60b1ae79f163883dd5
lazynezha <noahxfyi@icloud.com>
2022-10-27 16:50:10 +0800

    初识常量变量

---
+++ 2022.10.27/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 0;
+    int b = 0;
+    int sum = 0;
+    scanf("%d %d", &a, &b); //有空格必须输入空格
+    sum = a + b;
+    printf("sum = %d\n", sum);
+
+    return 0;
+}
