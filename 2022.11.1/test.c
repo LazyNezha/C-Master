a9cca8920f2c21495cb717ede64fffedf4c2fc8f
lazynezha <noahxfyi@icloud.com>
2022-11-02 16:43:43 +0800

    2022.11.2 12-14

---
+++ 2022.11.1/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+//    char arr[] = "hello";
+//    printf("\a\a\a");
+    printf("%d\n", strlen("c:/test/328/test.c"));// /328八进制里没8
+
+    return 0;
+}
