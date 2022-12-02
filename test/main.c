fb067811d511f13d0c6644c2debe301a76192351
LazyNezha <noahxfyi@icloud.com>
2022-12-02 16:08:50 +0800

    12.2 41

--- test/main.c
+++ test/main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
-
-struct Stu {
-    char name[20];
-    int age;
-    double score;
-};
+#include <math.h>
 
 int main () {
-    struct Stu s = {"张三", 20, 85.5};
-    printf("1: %s %d %lf\n", s.name, s.age, s.score);
-
-    struct Stu * ps = &s;
-    printf("2: %s %d %lf\n", (*ps).name, (*ps).age, (*ps).score);
-    printf("3: %s %d %lf\n", ps -> name, ps -> age, ps -> score);
+    int i = 0;
+    for (i = 100; i <= 200; i++) {
+        //判断i是否为素数
+        //2 -> i-1之间的数字去试除i，看能不能整除
+        int j = 0;
+        //m = a * b
+        //a和b中一定至少有一个数字是<=开平方m的
+        //16 = 2 * 8 = 4 * 4
+        int flag = 1;//假设i就是素数
+        for (j = 2; j <= sqrt(i); j++) {
+            if (i % j == 0) {
+                flag = 0;//不是素数
+                break;
+            }
+        }
+        if (flag == 1) {
+            printf("%d ", i);
+        }
+    }
 
     return 0;
 }
